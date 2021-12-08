0a98797000d70ef6840cede29a906f4d19a3aff5
katsu0212 <katsu02122728@gmail.com>
2021-12-08 10:27:02 +0900

    メッセージ送信機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @post = @room.posts.new(post_params)
+    if @post.save
+      redirect_to room_posts_path(@room)
+    else  
+      render :index
+    end
+  end
+
+  private
+  params.require(:post).permit(:content).merge(user_id: current_user.id)
 end
