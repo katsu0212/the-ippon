daec6eacd707086fcf1ceb59b7e9deb551006fcc
katsu0212 <katsu02122728@gmail.com>
2021-12-08 10:44:19 +0900

    メッセージの表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def index
     @post = Post.new
     @room = Room.find(params[:room_id])
+    @posts = @room.posts.includes(:user)
   end
 
   def create
@@ -10,10 +11,13 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to room_posts_path(@room)
     else  
+      @posts = @room.posts.includes(:user)
       render :index
     end
   end
 
-  private
-  params.require(:post).permit(:content).merge(user_id: current_user.id)
+  private 
+  def post_params
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
+  end
 end
